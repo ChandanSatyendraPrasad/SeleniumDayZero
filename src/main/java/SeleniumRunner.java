88ab938fdd746f5f05413c945d15b81939b503cb
91898 <chandansatyendraprasad@gmail.com>
2022-03-19 16:37:01 +0530

    WebDriver 1.0

--- src/main/java/SeleniumRunner.java
+++ src/main/java/SeleniumRunner.java
@@ -6,6 +6,8 @@ public class SeleniumRunner {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver webDriver= new ChromeDriver() ;
+        webDriver.get("https://www.google.com/");
+
 
     }
 }
